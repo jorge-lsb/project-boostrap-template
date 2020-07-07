4217074a1bfb975755c4d75409740f59b79c9e79
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-07-07 14:57:01 -0300

    Filtro com Jquery e JS/bootstrap grid projetos

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
     // PROGRESS BAR
 
     let containerA = document.getElementById("circleA");
 
-    let circleA = new ProgressBar.Circle(containerA,{
+    let circleA = new ProgressBar.Circle(containerA, {
 
-        color:'#64DAF9',
+        color: '#64DAF9',
         strokeWidth: 8,
         duration: 1400,
-        from :{ color:'#AAA' },
+        from: { color: '#AAA' },
         to: { color: '#65DAF9' },
 
-        step: function(state, circle) {
+        step: function (state, circle) {
 
             circle.path.setAttribute('stroke', state.color);
 
@@ -25,15 +25,15 @@ $(document).ready(function (){
 
     let containerB = document.getElementById("circleB");
 
-    let circleB = new ProgressBar.Circle(containerB,{
+    let circleB = new ProgressBar.Circle(containerB, {
 
-        color:'#64DAF9',
+        color: '#64DAF9',
         strokeWidth: 8,
         duration: 1600,
-        from :{ color:'#AAA' },
+        from: { color: '#AAA' },
         to: { color: '#65DAF9' },
 
-        step: function(state, circle) {
+        step: function (state, circle) {
 
             circle.path.setAttribute('stroke', state.color);
 
@@ -46,15 +46,15 @@ $(document).ready(function (){
 
     let containerC = document.getElementById("circleC");
 
-    let circleC = new ProgressBar.Circle(containerC,{
+    let circleC = new ProgressBar.Circle(containerC, {
 
-        color:'#64DAF9',
+        color: '#64DAF9',
         strokeWidth: 8,
         duration: 2000,
-        from :{ color:'#AAA' },
+        from: { color: '#AAA' },
         to: { color: '#65DAF9' },
 
-        step: function(state, circle) {
+        step: function (state, circle) {
 
             circle.path.setAttribute('stroke', state.color);
 
@@ -67,15 +67,15 @@ $(document).ready(function (){
 
     let containerD = document.getElementById("circleD");
 
-    let circleD = new ProgressBar.Circle(containerD,{
+    let circleD = new ProgressBar.Circle(containerD, {
 
-        color:'#64DAF9',
+        color: '#64DAF9',
         strokeWidth: 8,
         duration: 2200,
-        from :{ color:'#AAA' },
+        from: { color: '#AAA' },
         to: { color: '#65DAF9' },
 
-        step: function(state, circle) {
+        step: function (state, circle) {
 
             circle.path.setAttribute('stroke', state.color);
 
@@ -91,10 +91,10 @@ $(document).ready(function (){
     let dataAreaOffset = $('#data-area').offset();
     let stop = 0;
 
-    $(window).scroll(function(e){
+    $(window).scroll(function (e) {
         let scroll = $(window).scrollTop();
 
-        if(scroll > (dataAreaOffset.top - 500) && stop == 0){
+        if (scroll > (dataAreaOffset.top - 500) && stop == 0) {
 
             circleA.animate(1.0);
             circleB.animate(1.0);
@@ -107,11 +107,47 @@ $(document).ready(function (){
 
     // PARALLAX
 
-    setTimeout(function(){
+    setTimeout(function () {
 
-        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
-        $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+        $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
 
-    },250)
+    }, 250)
+
+    // FILTRO DO PORTIFOLIO
+
+    $('.filter-btn').on('click', function () {
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes)
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes)
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+    }
 
 });
