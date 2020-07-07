e7866358838c50437940f09700e3de3d99701f33
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-07-07 13:52:21 -0300

    Grid com parallax sessão trabalhe conosco

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,8 @@ $(document).ready(function (){
     setTimeout(function(){
 
         $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
-        
+        $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+
     },250)
 
 });
