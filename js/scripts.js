2505d14dea291bda9f971d643d1a2a1a96c0948e
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-07-08 16:27:30 -0300

    Finalizando projeto bootstrap menu ancora(jquery)

--- js/scripts.js
+++ js/scripts.js
@@ -150,4 +150,43 @@ $(document).ready(function () {
         }
     }
 
+    // SCROLL PARA SEÇÕES
+
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function (){
+        
+        let btnId = $(this).attr('id');
+
+        console.log(btnId);
+
+        if(btnId == 'about-menu'){
+            scrollTo = aboutSection
+        }else if (btnId == 'services-menu'){
+            scrollTo = servicesSection;
+        }else if (btnId == 'team-menu'){
+            scrollTo = teamSection;
+        }else if (btnId == 'portfolio-menu'){
+            scrollTo =portfolioSection;
+        }else if (btnId == 'contact-menu'){
+            scrollTo = contactSection;
+        }else{
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+    
+    });
+   
 });
